5623200fec2defe9ed851fefc964dee9cf2ab1a7
Randima <randimaabeywardhana2004@gmail.com>
2026-06-08 14:53:07 +0530

    Add files via upload

---
+++ grading system.py
@@ -0,0 +1,11 @@
+mark = float (input("Enter your mark :"))
+if mark>=75:
+    print ("Grade A")
+elif mark>=65:
+    print ("Grade B")
+elif mark>=55:
+    print ("Grade C")
+else:
+    print ("Grade F")
+    
+    
